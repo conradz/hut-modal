452b25756599812b9cee5cf2b8035073c77ca3bb
Conrad Zimmerman <me@conradz.com>
2013-11-07 19:08:20 +0000

    Upgrade hut-build and fix lint

--- example/example.js
+++ example/example.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /*global console */
 
 var Modal = require('../'),
--- test.js
+++ test.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var test = require('tape'),
     create = require('chi-create'),
     classes = require('chi-classes'),
