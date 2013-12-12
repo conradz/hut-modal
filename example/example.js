674e0c5fe201f348c6875647df24bd416ffc1e02
Conrad Zimmerman <me@conradz.com>
2013-12-12 13:55:22 +0000

    Better example

--- example/example.js
+++ example/example.js
@@ -1,18 +1,17 @@
 'use strict';
 
-/*global console */
-
 var Modal = require('../'),
     events = require('chi-events'),
     document = window.document;
 
 var modal = new Modal(document.querySelector('#example-modal')),
-    show = document.querySelector('#show-modal');
+    show = document.querySelector('#show-modal'),
+    status = document.querySelector('#status');
 
 events(show).on('click', function() {
     modal.show();
 });
 
 modal.on('result', function(result) {
-    console.log('Result: ' + result);
+    status.textContent = 'Result: ' + result;
 });
